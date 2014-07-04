d07e52cc1e5b66c5b9063cd5e76fc778963621b7
William Hockey <williamrhockey@gmail.com>
2014-07-04 12:04:13 -0700

    Users

---
+++ users.js
@@ -0,0 +1,24 @@
+module.exports = [
+	{
+    first_name: 'xxx',
+    last_name: 'xxx',
+	  email: 'xxx',
+	  phone_number: 'xxx',
+    messages: [],
+    cron: 'xxx',
+	}, {
+    first_name: 'xxx',
+    last_name: 'xxx',
+    email: 'xxx',
+    phone_number: 'xxx',
+    messages: [],
+    cron: 'xxx',
+  }, {
+    first_name: 'xxx',
+    last_name: 'xxx',
+    email: 'xxx',
+    phone_number: 'xxx',
+    messages: [],
+    cron: 'xxx',
+  }
+];
